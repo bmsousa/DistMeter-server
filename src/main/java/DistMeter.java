bbc85b25c538a59d203bfcd6a371cbc47210de39
Bruno Sousa <bmsousa@onesource.pt>
2018-05-18 10:22:56 +0100

    Add support for Test Server as discussed in meeting

--- src/main/java/DistMeter.java
+++ src/main/java/DistMeter.java
@@ -1,4 +1,5 @@
 import components.distconfmanager.DistConfManager;
+import components.disttestserver.DistmeterTestServer;
 import conf.ConfManager;
 
 public class DistMeter {
@@ -7,8 +8,11 @@ public class DistMeter {
         ConfManager confManager = new ConfManager(); // Configuration object
 
         //TODO: manage configuration flag
-        DistConfManager distmeterConfManager = new DistConfManager(confManager);
-        distmeterConfManager.run(args);
+        //DistConfManager distmeterConfManager = new DistConfManager(confManager);
+        //distmeterConfManager.run(args);
+
+        DistmeterTestServer distmeterTestServer = new DistmeterTestServer(confManager);
+        distmeterTestServer.run(args);
 
 
     }
